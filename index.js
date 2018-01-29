d9902c93cf39be921f84f03eca02f875478d1d5b
An Nuyen <annuyen@gmail.com>
2018-01-29 15:35:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array, callback) {
+  const newArray = [];
+
+  for (const element of array) {
+    newArray.push(callback(element));
+  }
+
+  return newArray;
+}
